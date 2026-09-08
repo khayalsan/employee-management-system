bf0cb4b3474e3afd481f9c702aac93a07099a098
KhayalAlizadeh <khayalalizadeh06@gmail.com>
2026-09-08 19:41:19 +0400

    Add entity exception and status classes

--- src/main/java/com/example/idtechexam2/entity/Employee.java
+++ src/main/java/com/example/idtechexam2/entity/Employee.java
@@ -1,4 +1,27 @@
 package com.example.idtechexam2.entity;
 
+import com.example.idtechexam2.enums.Status;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
 public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String firstName;
+	String lastName;
+	String email;
+	String phoneNumber;
+	String position;
+	Double salary;
+	LocalDate hireDate;
+	Status status;
 }
---
+++ src/main/java/com/example/idtechexam2/enums/Status.java
@@ -0,0 +1,7 @@
+package com.example.idtechexam2.enums;
+
+public enum Status {
+	ACTIVE,
+	INACTIVE,
+	ON_LEAVE
+}
